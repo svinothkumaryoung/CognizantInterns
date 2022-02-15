6099bc7d25f85c7713e576781994699ebb298ad3
svinothkumaryoung <vinchi.frnds@gmail.com>
2022-02-15 11:24:48 +0530

    Commit

---
+++ src/com/cognizantInterns/java8StudentPractice/InBuiltFunctionalInterface/ConsumerAndThen.java
@@ -0,0 +1,18 @@
+package com.cognizantInterns.java8StudentPractice.InBuiltFunctionalInterface;
+
+import java.util.function.Consumer;
+
+public class ConsumerAndThen {
+    static Consumer<String> c1=(a)->{
+        System.out.println("The Value Recieved is "+a);
+    };
+    static Consumer<String> c2=(b)->{
+        System.out.println("The Value Recieved is "+b);
+    };
+
+    public static void main(String[] args) {
+        //c1.accept("Vinothkumar ");
+        //c2.accept("Vinothkumar");
+        c1.andThen(c2).accept("Vinothkumar");
+    }
+}
